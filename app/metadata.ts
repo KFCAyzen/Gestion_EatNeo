19e814b05d6891cbf70157cecb869f51b7da9c92
KEPSEU Franck Celestin <kepseufrankc@gmail.com>
2025-11-07 12:36:27 +0100

    Add logo.jpg as favicon

--- app/metadata.ts
+++ app/metadata.ts
@@ -3,4 +3,7 @@ import type { Metadata } from 'next'
 export const metadata: Metadata = {
   title: 'EAT NEO',
   description: 'Restaurant et bar - Commandez en ligne',
+  icons: {
+    icon: '/logo.jpg',
+  },
 }
